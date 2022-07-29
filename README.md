2caba02afdbaa66db727a226c8876d97e640289a
rssabbir <sabbirhossainrahad@gmail.com>
2022-07-29 14:06:06 +0600

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# edu-hub-tailwind - is assginment 3 clone with tailwind css
---
+++ customUtility.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				clifford: "#da373d",
+			},
+			fontFamily: {
+				eduQ: ["Edu QLD Beginner", "cursive"],
+				inter: ["Inter", "sans-serif"],
+			},
+		},
+	},
+};
