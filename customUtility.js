44eeb8421d9208652a3aa0054903b97c7b664c22
rssabbir <sabbirhossainrahad@gmail.com>
2022-07-29 23:06:28 +0600

    full project done with tailwind

--- customUtility.js
+++ customUtility.js
@@ -2,11 +2,15 @@ tailwind.config = {
 	theme: {
 		extend: {
 			colors: {
-				clifford: "#da373d",
+				customPrimary: " #7FBF50",
+				customPrimaryLight: "#F4FAFD",
 			},
 			fontFamily: {
 				eduQ: ["Edu QLD Beginner", "cursive"],
 				inter: ["Inter", "sans-serif"],
+			},
+			screens: {
+				customMd: { min: "991px" },
 			},
 		},
 	},
